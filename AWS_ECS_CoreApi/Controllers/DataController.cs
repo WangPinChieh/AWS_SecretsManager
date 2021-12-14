d28e5827debaa2dc37353ca19cc9b62138e476ea
Jay Wang <jay.wang@hp.com>
2021-12-14 11:34:00 +0800

    remove bin and obj

--- AWS_ECS_CoreApi/Controllers/DataController.cs
+++ AWS_ECS_CoreApi/Controllers/DataController.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 using Amazon;
 using Amazon.Runtime.CredentialManagement;
 using Amazon.S3;
