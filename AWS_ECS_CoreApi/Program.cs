e73db28666c2012ceb4dafcca3e2e5366f679efe
Jay Wang <jay.wang@hp.com>
2021-12-21 14:37:28 +0800

    add HealthChecks

--- AWS_ECS_CoreApi/Program.cs
+++ AWS_ECS_CoreApi/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
+
 builder.Services
     .AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "My API", Version = "v1"}); })
     .AddControllers();
@@ -25,7 +27,9 @@ var app = builder.Build();
 app.UseRouting();
 app.UseSwagger();
 app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+app.MapHealthChecks("/health");
 app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
